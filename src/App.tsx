d4912b60779558af33652e46d03376df7830e2b8
yougyung <youkyeongs@naver.com>
2024-11-29 19:05:49 +0900

    feat: 파일 전송 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import UploadPDF from './components/common/upload-pdf/upload-pdf';
+import { useState } from 'react';
+import UploadPdf from './components/common/upload-pdf/upload-pdf';
+import './style/app.scss';
 
 function App() {
+  const [uploadFile, setUploadFile] = useState<File | null>(null);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const file = formData.get('file');
+    if (file && file instanceof File) {
+      setUploadFile(file);
+    }
+  };
+
   return (
     <div className="App">
-      <UploadPDF />
+      <form className="Submit" onSubmit={handleSubmit}>
+        <UploadPdf />
+        <button className="Submit__Button" type="submit">
+          결과 확인하기
+        </button>
+      </form>
     </div>
   );
 }
