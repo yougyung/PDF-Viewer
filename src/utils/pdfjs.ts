6cb3e55d8f3c2471f3332fa77f1534635b19b8b1
yougyung <youkyeongs@naver.com>
2024-12-01 22:55:01 +0900

    fix: init 함수명 변경

--- src/utils/pdfjs.ts
+++ src/utils/pdfjs.ts
@@ -4,14 +4,14 @@ import { getDocument } from 'pdfjs-dist';
 let pdfJsDidInit = false;
 const PDFJS_WORKER = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.10.377/pdf.worker.min.js';
 
-const pdfJsConnection = () => {
+const initializePdfJs = () => {
   if (!pdfJsDidInit) {
     GlobalWorkerOptions.workerSrc = PDFJS_WORKER;
   }
 };
 
 export const loadPdf = async (file: File) => {
-  pdfJsConnection();
+  initializePdfJs();
   const arrayBuffer = await file.arrayBuffer();
   const uint8Array = new Uint8Array(arrayBuffer);
   const pdf = await getDocument(uint8Array).promise;
